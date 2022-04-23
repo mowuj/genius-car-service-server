57c74fe36fd276cec110064a03d1dcc947abab78
mowuj <mowuj92@gmail.com>
2022-04-23 15:55:37 +0600

    connent to db and use dotenv create a get api

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const res = require('express/lib/response');
+require('dotenv').config();
+const app = express();
+const port= process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connect to database 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jhijo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusCar').collection('service');
+        
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+        
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir);
+
+// get api
+app.get('/', (req, res) => {
+    res.send('running genius Server');
+});
+
+app.listen(port, () => {
+    console.log('Listening to port',port);
+})
